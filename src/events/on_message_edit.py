be89c87cd7f14f3dbbfe6a2e44f5bd17fe76e542
Mikk155 <martimsanchez155@gmail.com>
2025-06-26 22:06:42 -0300

    fix syntax issue

--- src/events/on_message_edit.py
+++ src/events/on_message_edit.py
@@ -3,4 +3,4 @@ from src.main import *;
 @bot.event
 async def on_message_edit( before: discord.Message, after: discord.Message ):
 
-    await g_PluginManager.CallFunction( "OnMessageEdited", before, after, GuildID=message.guild.id );
+    await g_PluginManager.CallFunction( "OnMessageEdited", before, after, GuildID=after.guild.id );
