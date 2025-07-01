95688206d322d474cdb59a87047d9229837e7882
Mikk155 <martimsanchez155@gmail.com>
2025-07-01 11:21:12 -0300

    plugin role_restorer

---
+++ plugins/role_restorer.py
@@ -0,0 +1,58 @@
+from src.main import *
+from src.PluginManager import Plugin;
+
+class role_restorer( Plugin ):
+
+    @property
+    def GetName(self):
+        return "Role restorer";
+
+    @property
+    def GetDescription(self):
+        return "Keep track of user roles when they leave a server to add them back when the user rejoin";
+
+    async def OnMemberLeave( self, user ):
+
+        if not user.guild:
+            return True;
+
+        GuildID = str( user.guild.id );
+
+        cache = g_Cache.Get();
+
+        GuildCache = cache.get( GuildID, {} );
+
+        Roles = [ Role.id for Role in user.roles if Role ];
+
+        GuildCache[ str( user.id ) ] = Roles;
+
+        cache[ GuildID ] = GuildCache;
+
+        return True;
+
+    async def OnMemberJoin( self, user ):
+
+        if not user.guild:
+            return True;
+
+        GuildID = str( user.guild.id );
+
+        cache = g_Cache.Get();
+
+        GuildCache = cache.get( GuildID, {} );
+
+        RolesID = GuildCache.pop( str( user.id ), {} );
+
+        cache[ GuildID ] = GuildCache;
+
+        Roles = [ Role for Role in user.guild.roles if Role.id in RolesID ];
+
+        for Role in Roles:
+
+            try: # One by one iteration due in case of one failing mid-way
+
+                await user.add_roles( Role );
+
+            except: pass;
+
+        return True;
