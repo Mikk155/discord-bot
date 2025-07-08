bf011f0fa3e9ac002cce9808038528ca720e2f2a
Mikk155 <martimsanchez155@gmail.com>
2025-07-08 13:52:00 -0300

    Update server_logs

--- plugins/server_logs.py
+++ plugins/server_logs.py
@@ -22,11 +22,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE
 '''
 
+from discord import Embed
+from discord import Embed
+from discord.components import SelectOption
+from discord.guild import Guild
 from project import *
 
-import difflib;
+import difflib
 
-#-TODO Maybe move this to somewhere else? May be util to other plugins
+from utils.Dictionary import Dictionary;
+
+
+class LogType(IntEnum):
+    MessageDeleted: int = 0;
+    MessageEdited: int = 1;
+    __LogsTypes__: int = 2;
+    '''This is used to set the size. must be the same value as the last enumerated object'''
+
 class ChannelSelect( discord.ui.Select ):
 
     def __init__( self,
@@ -35,26 +47,27 @@ class ChannelSelect( discord.ui.Select ):
         Guild: discord.Guild,
         fnOnUpdate: Callable[ [str, int, int], Awaitable[True] ] = None
     ):
+    #
+        self.fnOnUpdate: Callable[ [str, int, int], Awaitable[True] ] = fnOnUpdate;
+        self.ItemName: str = ItemName;
+        self.Guild: discord.Guild = Guild;
 
-        self.fnOnUpdate = fnOnUpdate;
-
-        self.ItemName = ItemName;
-
-        self.Guild = Guild;
-
-        options = [
+        options: list[SelectOption] = [
             discord.SelectOption( label = channel.name, value = str( channel.id ) )
             for channel in Guild.text_channels
         ];
 
         super().__init__( placeholder=Title, options=options, min_values=1, max_values=1 );
+    #
 
     async def callback( self, interaction: discord.Interaction ):
-
+    #
         if self.fnOnUpdate is not None:
-
-            await self.fnOnUpdate( self.ItemName, int( self.values[0] ), self.Guild );
+        #
+            await self.fnOnUpdate( self.ItemName, int( self.values[0] ), self.Guild.id );
             await interaction.response.send_message( "✅", ephemeral=True, delete_after=1.5 );
+        #
+    #
 
 class ConfigView( discord.ui.View ):
 
@@ -64,28 +77,32 @@ class ConfigView( discord.ui.View ):
         fnOnUpdate: Callable[ [str, int, int], Awaitable[True] ] = None,
         fnOnTimeOut: Callable[ [discord.ui.View], Awaitable[True] ] = None,
     ):
+    #
         '''
             fnOnUpdate called when a option is updated. the first argument is a integer and is the channel id
         '''
-
         super().__init__( timeout=300 );
-
         self.guild = Guild
         self.fnOnTimeOut = fnOnTimeOut;
 
         for k, v in Items.items():
-
+        #
             self.add_item( ChannelSelect( k, v, Guild, fnOnUpdate ) );
+        #
+    #
 
     async def on_timeout(self):
-
+    #
         if self.fnOnTimeOut is not None:
+        #
             await self.fnOnTimeOut(self);
+        #
+    #
 
 class server_logs( Plugin ):
 
     def OnPluginActivate(self):
-
+    #
         command = app_commands.Command(
             name="cfg_loggin",
             description="Configure the server-loggin messages",
@@ -93,234 +110,333 @@ class server_logs( Plugin ):
         );
 
         command.guild_only = True;
-
         bot.tree.add_command( command );
-
         g_Sentences.push_back( "server_logs" );
+    #
 
     def OnPluginDeactivate(self):
-
+    #
         bot.tree.remove_command( "cfg_loggin" );
+    #
 
     @property
     def GetName(self):
+    #
         return "Server loggin";
+    #
 
     @property
     def GetDescription(self):
+    #
         return "Keep track of messages deleted, edited, audit log entries and more";
+    #
 
-    LogTypes = (
-        "OnMessageEdited",
-        "OnMessageDelete"
-    );
+    def SetDefaultLabels( self, cache: Dictionary ) -> None:
+    #
+        if cache.IsEmpty:
+        #
+            cache[ "log_channels" ] = [];
+            cache[ "filter_channels" ] = [];
+        #
 
-    async def OnMessageEdited(self, before, after):
+        while len(cache[ "log_channels" ]) < LogType.__LogsTypes__:
+        #
+            cache[ "log_channels" ].append( None );
+        #
+    #
 
-        if after.author.id == bot.user.id or not after.guild:
-            return True;
+    async def OnMessageEdited( self, before: discord.Message, after: discord.Message ) -> Hook:
+    #
+        channel: discord.TextChannel = after.channel;
 
-        GuildID = after.guild.id;
+        if isinstance( channel, discord.GroupChannel ) or isinstance( channel, discord.DMChannel ):
+        #
+            return Hook.Continue;
+        #
 
-        cache = g_Cache.Get();
+        cache: Dictionary = g_Cache.Plugin;
 
-        channel = bot.get_channel( cache.get( str( GuildID ), {} ).get( self.LogTypes[0], 0 ) );
+        if not after.guild or not after.guild.id in cache:
+        #
+            return Hook.Continue;
+        #
 
-        if not channel or channel is None:
-            return True;
+        GuildCache: Dictionary = cache[ after.guild.id ];
+
+        self.SetDefaultLabels( GuildCache );
+
+        if after.channel.id in GuildCache[ "filter_channels" ]:
+        #
+            return Hook.Continue;
+        #
+
+        LogTypes: list[LogType] = GuildCache[ "log_channels" ];
+
+        if LogTypes[LogType.MessageEdited] is None:
+        #
+            return Hook.Continue;
+        #
+
+        channel = bot.get_channel( LogTypes[LogType.MessageEdited] );
+
+        if not channel or channel is None or isinstance( channel, discord.GroupChannel ) or isinstance( channel, discord.DMChannel ):
+        #
+            return Hook.Continue;
+        #
 
         embed = discord.Embed(
             color = RGB(100,255,0).hex,
-            title = g_Sentences.get( "server_logs_cfg_OnMessageEdited", Guild=GuildID ),
-            description = g_Sentences.get( "server_logs_cfg_OnMessageEdited_info", after.author.mention, after.jump_url, Guild=GuildID )
+            title = g_Sentences.get(
+                "server_logs_cfg_MessageEdited",
+                Guild=after.guild
+            ),
+            description = g_Sentences.get(
+                "server_logs_cfg_MessageEdited_info",
+                after.author.mention,
+                after.jump_url,
+                Guild=after.guild
+            )
         );
 
-        old = before.content;
-        new = after.content;
+        old: str = before.content;
+        new: str = after.content;
 
-        matcher = difflib.SequenceMatcher( None, old, new );
-        old_result = [];
-        new_result = [];
+        matcher: difflib.SequenceMatcher[str] = difflib.SequenceMatcher( None, old, new );
+        old_result: list[str] = [];
+        new_result: list[str] = [];
 
         for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+        #
             if tag == 'equal':
+            #
                 old_result.append(old[i1:i2]);
                 new_result.append(new[j1:j2]);
+            #
             elif tag == 'replace':
+            #
                 old_result.append(f"<replace old>{old[i1:i2]}<>");
                 new_result.append(f"<replace new>{new[j1:j2]}<>");
+            #
             elif tag == 'delete':
+            #
                 old_result.append(f"<deleted>{old[i1:i2]}<>");
+            #
             elif tag == 'insert':
+            #
                 new_result.append(f"<inserted>{new[j1:j2]}<>");
+            #
+        #
 
-        old = ''.join( o for o in old_result );
-        new = ''.join( o for o in new_result );
+        old: str = ''.join( o for o in old_result );
+        new: str = ''.join( o for o in new_result );
 
         if old != new:
-
+        #
             def TuncateEmbeds( _newEmbed: discord.Embed, _Text: str, _Sentence: str, _SentenceParts: str ) -> discord.Embed:
-
-                _CurrentPart = 1;
+            #
+                _CurrentPart: int = 1;
 
                 if len(_Text) > 1024:
-
+                #
                     while len( _Text ) > 1024:
-
-                        _Text_part = _Text[ : 1024 ];
-
+                    #
+                        _Text_part: str = _Text[ : 1024 ];
                         _Text = _Text[ 1024 : ];
 
                         _newEmbed.add_field( inline = False,
-                            name = g_Sentences.get( _SentenceParts, _CurrentPart, Guild=GuildID ),
+                            name = g_Sentences.get( _SentenceParts, _CurrentPart, Guild=after.guild ),
                             value = _Text_part
                         );
-
                         _CurrentPart = _CurrentPart + 1;
+                    #
+                #
 
                 if len( _Text ) > 0:
-
+                #
                     _newEmbed.add_field( inline = False,
-                        name = g_Sentences.get( _Sentence, Guild=GuildID ) if _CurrentPart > 1 else g_Sentences.get( _Sentence, _SentenceParts, Guild=GuildID ),
+                        name = g_Sentences.get( _Sentence, Guild=after.guild ) if _CurrentPart > 1 else g_Sentences.get( _Sentence, _SentenceParts, Guild=after.guild ),
                         value = _Text
                     );
+                #
 
                 return _newEmbed;
-
-            embed = TuncateEmbeds( embed, old, "server_logs_cfg_OnMessageEdited_Before", "server_logs_cfg_OnMessageEdited_BeforePart" );
-            embed = TuncateEmbeds( embed, new, "server_logs_cfg_OnMessageEdited_New", "server_logs_cfg_OnMessageEdited_NewPart" );
-
+            #
+            embed: Embed = TuncateEmbeds(
+                embed, old,
+                "server_logs_cfg_MessageEdited_Before",
+                "server_logs_cfg_MessageEdited_BeforePart"
+            );
+            embed: Embed = TuncateEmbeds(
+                embed, new,
+                "server_logs_cfg_MessageEdited_New",
+                "server_logs_cfg_MessageEdited_NewPart"
+            );
             await channel.send( embed=embed, allowed_mentions=False, mention_author=False );
+        #
+        return Hook.Continue;
+    #
 
-        return True;
-
-    async def OnLogTypeUpdated( self, item: str, channel: int, Guild: int ):
-
+    async def OnLogTypeUpdated( self, item: str, channel: int, Guild: int ) -> None:
+    #
         if Guild is None:
+        #
             return;
+        #
 
-        cache = g_Cache.Get();
+        GuildCache: Dictionary = g_Cache.Plugin[ Guild ];
 
-        GuildConfig = cache.get( str( Guild.id ), {} );
+        self.SetDefaultLabels(GuildCache);
 
-        GuildConfig[ item ] = channel;
+        if GuildCache.IsEmpty:
+        #
+            GuildCache[ "log_channels" ] = [ None, None ];
+            GuildCache[ "filter_channels" ] = [];
+        #
 
-        cache[ str( Guild.id ) ] = GuildConfig;
+        ItemIndex: list[LogType] = [ T.value for T in LogType if T.name == item ];
 
-    async def OnMessageDelete( self, message, deleter ):
+        GuildCache[ "log_channels" ][ItemIndex[0]] = channel;
+    #
 
-        if message.author.id == bot.user.id or not message.guild:
-            return True;
+    async def OnMessageDelete( self, message: discord.Message, deleter: Union[discord.User | discord.Member] ) -> Hook:
 
-        GuildID = message.guild.id;
+        channel: discord.TextChannel = message.channel;
 
-        cache = g_Cache.Get();
+        if isinstance( channel, discord.GroupChannel ) or isinstance( channel, discord.DMChannel ):
+        #
+            return Hook.Continue;
+        #
 
-        channel = bot.get_channel( cache.get( str( GuildID ), {} ).get( self.LogTypes[1], 0 ) );
+        cache: Dictionary = g_Cache.Plugin;
 
-        if not channel or channel is None:
-            return;
+        if not message.guild or not message.guild.id in cache:
+        #
+            return Hook.Continue;
+        #
 
-        AuthorMSG = message.author.mention;
+        GuildCache: Dictionary = cache[ message.guild.id ];
 
-        if message.webhook_id is not None:
+        self.SetDefaultLabels( GuildCache );
 
-            WebHookAt = [ w for w in await message.channel.webhooks() if w.id == message.webhook_id ];
+        if message.channel.id in GuildCache[ "filter_channels" ]:
+        #
+            return Hook.Continue;
+        #
+
+        LogTypes: list[LogType] = GuildCache[ "log_channels" ];
+
+        if LogTypes[LogType.MessageDeleted] is None:
+        #
+            return Hook.Continue;
+        #
+
+        channel = bot.get_channel( LogTypes[LogType.MessageDeleted] );
+
+        if not channel or channel is None or isinstance( channel, discord.GroupChannel ) or isinstance( channel, discord.DMChannel ):
+        #
+            return Hook.Continue;
+        #
+
+        AuthorMSG: str = message.author.mention;
+
+        if message.webhook_id is not None: # Is this a webhook?
+        #
+            WebHookAt: list[discord.Webhook] = [ w for w in await message.channel.webhooks() if w.id == message.webhook_id ];
 
             if len(WebHookAt) > 0:
+            #
                 AuthorMSG = f'{AuthorMSG} (Webhook ``{WebHookAt[0].name}``)';
+            #
+        #
 
         embed = discord.Embed(
             color = RGB(255,0,0).hex,
-            title=g_Sentences.get( "server_logs_cfg_OnMessageDelete", Guild=GuildID ),
-            description=g_Sentences.get( "server_logs_message_deleted_info", AuthorMSG, message.channel.jump_url, Guild=GuildID )
+            title=g_Sentences.get( "server_logs_cfg_MessageDelete", Guild=message.guild ),
+            description=g_Sentences.get( "server_logs_message_deleted_info", AuthorMSG, message.channel.jump_url, Guild=message.guild )
         );
 
         try:
-            replied = await message.channel.fetch_message( message.reference.message_id );
+        #
+            replied: discord.Message = await message.channel.fetch_message( message.reference.message_id );
+
             if replied:
-                embed.description = embed.description + '\n' + g_Sentences.get( "server_logs_message_deleted_replied", replied.jump_url, Guild=GuildID );
+            #
+                embed.description = embed.description + '\n' + g_Sentences.get( "server_logs_message_deleted_replied", replied.jump_url, Guild=message.guild );
+            #
+        #
         except:
+        #
             pass;
+        #
 
         if message.content:
-
+        #
             content = message.content;
 
             if len(content) > 1024:
-
-                part = 1;
+            #
+                part: int = 1;
 
                 while len( content ) > 1024:
-
+                #
                     content_part = content[ : 1024 ];
                     content = content[ 1024 : ];
 
                     embed.add_field( inline = False,
-                        name =g_Sentences.get( "server_logs_message_deleted_content", part, Guild=GuildID ),
+                        name =g_Sentences.get( "server_logs_message_deleted_content", part, Guild=message.guild ),
                         value = content_part
                     );
 
                     part = part + 1;
-
+                #
                 if len(content) > 0:
-
+                #
                     embed.add_field( inline = False,
-                        name =g_Sentences.get( "content", Guild=GuildID ),
+                        name =g_Sentences.get( "content", Guild=message.guild ),
                         value = content
                     );
-
+                #
+            #
             else:
-
+            #
                 embed.add_field( inline = False,
-                    name =g_Sentences.get( "content", Guild=GuildID ),
+                    name =g_Sentences.get( "content", Guild=message.guild ),
                     value = content
                 );
+            #
+        #
 
         if deleter.id != message.author.id:
-
+        #
             embed.add_field( inline = False,
-                name = g_Sentences.get( "server_logs_message_deleted_by", Guild=GuildID ),
+                name = g_Sentences.get( "server_logs_message_deleted_by", Guild=message.guild ),
                 value = fmt.DiscordUserMention(deleter)
             );
+        #
 
         if message.embeds and len(message.embeds) > 0:
-
-            embeds = message.embeds;
-
+        #
+            embeds: list[discord.Embed] = message.embeds;
             embeds.insert( 0, embed );
-
             await channel.send( embeds=embeds, allowed_mentions=False, mention_author=False, silent=True );
-
+        #
         else:
-
+        #
             await channel.send( embed=embed, allowed_mentions=False, mention_author=False, silent=True );
+        #
+        return Hook.Continue;
+    #
 
-        return True;
-
+    @Plugin.HandleExceptions()
     @app_commands.describe()
     @app_commands.default_permissions( administrator=True )
     async def command_cfg_loggin( self, interaction: discord.Interaction ):
+    #
+        plugin: server_logs = g_PluginManager.GetCurrentPlugin;
 
-        try:
+        Items: dict[str, str] = { T.name: g_Sentences.get( f'server_logs_cfg_{T.name}', Guild=interaction.guild ) for T in LogType };
 
-            from src.PluginManager import g_PluginManager;
-            plugin: server_logs = g_PluginManager.GetCurrentPlugin;
+        UiView = ConfigView( Items, interaction.guild, plugin.OnLogTypeUpdated );
 
-            Items = { k: g_Sentences.get( f'server_logs_cfg_{k}', Guild=interaction.guild ) for k in plugin.LogTypes };
-
-            UiView = ConfigView( Items, interaction.guild, plugin.OnLogTypeUpdated );
-
-            await interaction.response.send_message( "server_logs_cfg_select", view=UiView );
-
-        except Exception as e:
-
-            from src.Bot import bot;
-
-            if interaction.response.is_done():
-
-                await interaction.followup.send( embeds=bot.HandleException( e, SendToDevs=True ) );
-
-            else:
-
-                await interaction.response.send_message( embeds=bot.HandleException( e, SendToDevs=True ) );
+        await interaction.response.send_message( "server_logs_cfg_select", view=UiView );
+    #
