b0edeaf6ce2e577a6d121fed3cde3e9cfd232a4d
Mikk155 <martimsanchez155@gmail.com>
2025-07-09 14:12:30 -0300

    Add role menu

--- plugins/role_menu.py
+++ plugins/role_menu.py
@@ -1,46 +1,13 @@
-'''
-MIT License
-
-Copyright (c) 2025 Mikk
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE
-'''
-
-from datetime import datetime
 from project import *
-from utils.Dictionary import Dictionary
 
 class role_menu( Plugin ):
-
-    MaxMenus = 5;
-
-    TimeoutConfiguration: float = 500;
-
-    ConfiguringMessages: list[ tuple[ int, int, int, datetime ] ] = [];
-    '''Contains message ID, channel ID and guild ID of a message that is currently being configured'''
 
     def OnPluginActivate(self) -> None:
     #
         command = app_commands.Command(
-            name="cfg_role_menu",
+            name="roles",
             description="Create a menu on wich users can choose roles",
-            callback=self.command_cfg_role_menu,
+            callback=self.command_roles,
         );
 
         command.guild_only = True;
@@ -52,7 +19,7 @@ class role_menu( Plugin ):
 
     def OnPluginDeactivate(self) -> None:
     #
-        bot.tree.remove_command( "cfg_role_menu" );
+        bot.tree.remove_command( "roles" );
     #
 
     @property
@@ -67,390 +34,130 @@ class role_menu( Plugin ):
         return "Create a menu on wich users can choose roles";
     #
 
-    async def OnThink( self, time: datetime ) -> Hook:
-    #
-        now: datetime = datetime.now() - timedelta( seconds=self.TimeoutConfiguration ) ;
-
-        cache = g_Cache.Plugin;
-
-        i = -1;
-
-        for C in self.ConfiguringMessages.copy():
-        #
-            i += 1;
-
-            if C[3] is None or now > C[3]:
-            #
-                cacheMessage: dict = cache[ C[2] ][ C[0] ];
-
-                if not "roles" in cacheMessage or len( cacheMessage[ "roles" ] ) == 0:
-                #
-                    cache[ C[2] ].pop( C[0] );
-                    self.ConfiguringMessages.pop(i);
-                    i -= 1;
-
-                    try:
-                    #
-                        msg: discord.Message = await bot.get_channel( C[1] ).fetch_message( C[0] );
-                        await msg.delete();
-                    #
-                    except: pass;
-                #
-            #
-        #
-        return Hook.Continue;
-    #
-
     class CRoleSelectionView( discord.ui.View ):
     #
-        def __init__( self, roles: dict[ str, str ], title: str ):
+        def __init__( self, Buttons: Dictionary, fnCallback, ExpireTime = None ):
         #
-            super().__init__( timeout=None );
+            if ExpireTime is None:
+                ExpireTime = Buttons[ "life" ];
 
-            Options: list[ discord.SelectOption ] = [];
+            super().__init__( timeout=ExpireTime );
 
-            for role, info in roles.items():
+            for name, button in Buttons.items():
             #
-                Options.append( discord.SelectOption( label = info[0], value = role, description = info[1] ) );
-            #
+                if isinstance( button, Dictionary ):
+                    button: dict = button.ToDict;
 
-            Selection = discord.ui.Select( placeholder=title, options=Options );
-
-            Selection.callback = self.SelectRole;
-
-            self.add_item( Selection );
-        #
-
-        async def SelectRole( self, interaction: discord.Interaction ) -> None:
-        #
-            try:
-            #
-                role: discord.Role = interaction.guild.get_role( int( interaction.data[ "values" ][0] ) )
-
-                if role in interaction.user.roles:
-                #
-                    await interaction.user.remove_roles( role );
-
-                    await interaction.response.send_message(
-                        g_Sentences.get( "role_menu_removed_role", role.mention, Guild=interaction.guild_id ),
-                        ephemeral=True,
-                        allowed_mentions=False
-                    );
-                #
-                else:
-                #
-                    await interaction.user.add_roles( role );
-
-                    await interaction.response.send_message(
-                        g_Sentences.get( "role_menu_added_role",
-                            role.mention,
-                            Guild=interaction.guild_id
-                        ),
-                        ephemeral=True,
-                        allowed_mentions=False
-                    );
-                #
-            #
-            except Exception as e:
-            #
-                from src.Bot import bot;
-
-                if interaction.response.is_done():
-                #
-                    await interaction.followup.send( embeds=bot.HandleException( e, SendToDevs=True ) );
-                #
-                else:
-                #
-                    await interaction.response.send_message( embeds=bot.HandleException( e, SendToDevs=True ) );
-                #
-            #
-        #
-    #
-
-    async def UpdateMenu( self, _c: dict, _m: discord.Message ) -> None:
-    #
-        Roles: dict[ str, str ] = _c.get( "roles", {} );
-
-        view: CRoleSelectionView = self.CRoleSelectionView( Roles, _c.get( "button", "Select a role" ) ) if len( Roles ) > 0 else None;
-
-        embed = discord.Embed(
-            color=RGB(0,100,0).hex,
-            title=_c.get( "name", "" ),
-            description=_c.get( "description", "" )
-        ) if "name" in _c else None;
-
-        await _m.edit( content='', embed=embed, view=view );
-    #
-
-    async def TrackMessages( self, Guild: discord.Guild, target: Optional[ Union[ int, discord.Message ] ] = 0 ):
-    #
-        cache: Dictionary = g_Cache.Plugin;
-
-        GuildID: int = Guild.id if isinstance( Guild, discord.Guild ) else Guild;
-
-        GuildCache: Dictionary = cache[ GuildID ];
-
-        if target == 0:
-        #
-            for k, v in GuildCache.items():
-            #
-                if not "roles" in v or len( v[ "roles" ] ) == 0:
-                #
-                    self.ConfiguringMessages.append( ( int(k), v[ "channel" ], GuildID, None ) );
+                if not isinstance( button, dict ):
                     continue;
-                #
-                try:
-                #
-                    channel: discord.TextChannel = bot.get_channel( v[ "channel" ] );
 
-                    message: discord.Message = await channel.fetch_message( int(k) );
+                Roles: dict[str, str] = button[ "roles" ];
 
-                    await self.UpdateMenu( v, message );
-                #
-                except: pass;
+                Options: list[ discord.SelectOption ] = [ discord.SelectOption( label = info, value = role ) for role, info in Roles.items() ];
+
+                Selection = discord.ui.Select( placeholder=button[ "title" ], options=Options );
+
+                Selection.callback = fnCallback;
+
+                self.add_item( Selection );
             #
         #
-        elif isinstance( target, discord.Message ):
+
+    @Plugin.HandleExceptions()
+    async def SelectRole( self, interaction: discord.Interaction ) -> None:
+    #
+        role: discord.Role = interaction.guild.get_role( int( interaction.data[ "values" ][0] ) )
+
+        if role in interaction.user.roles:
         #
-            await self.UpdateMenu( GuildCache[ str( target.id ) ], target );
+            await interaction.user.remove_roles( role );
+
+            await interaction.response.send_message(
+                g_Sentences.get(
+                    "role_menu_removed_role",
+                    role.mention,
+                    interaction.user.mention,
+                    Guild=interaction.guild_id
+                ),
+                allowed_mentions=False,
+                delete_after=5,
+                silent=True
+            );
         #
         else:
         #
-            k: Dictionary = GuildCache[ str( target ) ];
-
-            channel = bot.get_channel( k[ "channel" ] );
-
-            message: discord.Message = await channel.fetch_message( str( target ) );
-
-            await self.UpdateMenu( k, message );
-        #
-    #
-
-    async def OnBotStart( self ) -> Hook:
-    #
-        for Guild in bot.guilds:
-        #
-            await self.TrackMessages(Guild);
-        #
-        return Hook.Continue;
-    #
-
-    async def OnReconnect( self ) -> Hook:
-    #
-        for Guild in bot.guilds:
-        #
-            await self.TrackMessages(Guild);
-        #
-        return Hook.Continue;
-    #
-
-    class ConfigModal( discord.ui.Modal, title="" ):
-    #
-        def __init__( self, interaction: discord.Interaction, menu: discord.Message, owner: Plugin ) -> None:
-        #
-            super().__init__( timeout=owner.TimeoutConfiguration );
-
-            self.interaction = interaction;
-            self.menu = menu;
-            self.owner: role_menu = owner;
-
-            self.ItemName = discord.ui.TextInput(
-                label=g_Sentences.get( "name", Guild=interaction.guild_id ),
-                placeholder=g_Sentences.get( "role_menu_embed_title", Guild=interaction.guild_id ),
-                max_length=100
-            );
-
-            self.add_item( self.ItemName );
-
-            self.ItemDescription = discord.ui.TextInput(
-                label=g_Sentences.get( "description", Guild=interaction.guild_id ),
-                placeholder=g_Sentences.get( "role_menu_embed_description", Guild=interaction.guild_id ),
-                max_length=100
-            );
-
-            self.add_item( self.ItemDescription );
-
-            self.ItemButton = discord.ui.TextInput(
-                label=g_Sentences.get( "role_menu_selection_title", Guild=interaction.guild_id ),
-                placeholder=g_Sentences.get( "role_menu_selection_description", Guild=interaction.guild_id ),
-                max_length=100
-            );
-
-            self.add_item( self.ItemButton );
-
-            self.owner.ConfiguringMessages.append( ( self.menu.id, self.menu.channel, self.interaction.guild_id, datetime.now() ) );
-        #
-
-        async def on_submit( self, interaction: discord.Interaction ):
-        #
-            cache: Dictionary = g_Cache.Plugin[ self.interaction.guild_id ][ self.menu.id ];
-
-            cache[ "name" ] = self.ItemName.value;
-            cache[ "description" ] = self.ItemDescription.value;
-            cache[ "button" ] = self.ItemButton.value;
-            cache[ "channel" ] = self.interaction.channel_id;
-
-            await self.owner.UpdateMenu( cache.ToDict, self.menu );
+            await interaction.user.add_roles( role );
 
             await interaction.response.send_message(
-                    g_Sentences.get( "role_menu_starting",
-                    self.owner.TimeoutConfiguration,
-                    self.menu.id,
+                g_Sentences.get( "role_menu_added_role",
+                    role.mention,
+                    interaction.user.mention,
                     Guild=interaction.guild_id
                 ),
-                silent=True,
                 allowed_mentions=False,
-                delete_after=self.owner.TimeoutConfiguration
+                delete_after=5,
+                silent=True
             );
         #
+        # -TODO How to reset this without recreating the view class and updating the message?
     #
 
-    @Plugin.HandleExceptions()
-    @app_commands.guild_only()
-    @app_commands.default_permissions( administrator=True )
-    @app_commands.describe(
-        menu='Message ID to alter or to delete if not role provided',
-        role='Role to add to the current configuring message.',
-        description='Description for the role'
-    )
-    async def command_cfg_role_menu(
-        self,
-        interaction: discord.Interaction,
-        role: Optional[ discord.Role ] = None,
-        description: Optional[ str ] = None,
-        menu: Optional[ str ] = None
-    ):
+    InteractionsOnChannels: dict[ int, datetime ] = {};
+
+    async def OnCommand( self, message: discord.Message, command: str, args: list[str] ) -> Hook:
     #
-        '''Create a role menu. run with no arguments to start configuring'''
+        if command != 'roles':
+            return Hook.Continue;
 
-        UserRoleMenu: discord.Message = None;
+        if message.author.id == bot.user.id:
+            return Hook.Break;
 
-        if menu is not None:
+        channel: discord.TextChannel = message.channel;
+
+        if isinstance( channel, discord.GroupChannel ) or isinstance( channel, discord.DMChannel ):
+            return Hook.Break;
+
+        await self.RoleMenuInteraction( channel );
+        return Hook.Break;
+    #
+
+    async def RoleMenuInteraction( self, channel: discord.Message ) -> None:
+    #
+        now: datetime = datetime.now();
+
+        cache: Dictionary = g_Cache.Plugin;
+
+        if channel.id in self.InteractionsOnChannels:
         #
-            try:
+            before: datetime = self.InteractionsOnChannels[ channel.id ];
+
+            Elapsed: timedelta = ( now - before );
+
+            Remaining: int = int( cache[ "life" ] - Elapsed.total_seconds() );
+
+            if Remaining > 0:
             #
-                UserRoleMenu: discord.Message = await interaction.channel.fetch_message( int(menu) );
-            #
-            except:
-            #
-                await interaction.response.send_message(
-                    g_Sentences.get( "role_menu_failed_find",
-                        menu,
-                        interaction.channel.jump_url,
-                        Guild=interaction.guild_id
-                    ),
-                    silent=True,
-                    allowed_mentions=False
+                await channel.send(
+                    "There's already a menu interaction in this channel. it will timeout in {} seconds".format( Remaining ),
+                    allowed_mentions=False,
+                    mention_author=False,
+                    silent=True
                 );
-                pass;
-            #
-
-            if role is None:
-            #
-                cacheGuild: Dictionary = g_Cache.Plugin[ self.interaction.guild_id ];
-
-                if str( UserRoleMenu.id ) in cacheGuild:
-                #
-                    cacheGuild.pop( str( UserRoleMenu.id ) );
-
-                    await UserRoleMenu.delete();
-
-                    await interaction.response.send_message(
-                        g_Sentences.get( "message_deleted", Guild=interaction.guild_id ),
-                        silent=True,
-                        allowed_mentions=False
-                    );
-                #
-                else:
-                #
-                    await interaction.response.send_message(
-                        g_Sentences.get( "role_menu_no_cache",
-                            UserRoleMenu.id,
-                            Guild=interaction.guild_id
-                        ),
-                        silent=True,
-                        allowed_mentions=False
-                    );
-                #
+                return;
             #
             else:
             #
-                cache: Dictionary = g_Cache.Plugin[ self.interaction.guild_id ][ menu ];
-
-                if str( role.id ) in cache[ "roles" ]:
-                #
-                    cache[ "roles" ].pop( str( role.id ) );
-                    await interaction.response.send_message(
-                        g_Sentences.get( "role_menu_removed_role",
-                            role.mention,
-                            Guild=interaction.guild_id
-                        ),
-                        silent=True,
-                        allowed_mentions=False
-                    );
-                #
-                elif not role.is_assignable():
-                #
-                    await interaction.response.send_message(
-                        g_Sentences.get( "role_menu_bad_hierarchy",
-                            role.mention,
-                            Guild=interaction.guild_id
-                        ),
-                        silent=True,
-                        allowed_mentions=False
-                    );
-                    return;
-                #
-                else:
-                #
-                    cache[ str( role.id ) ] = [ role.name, description ];
-
-                    await interaction.response.send_message(
-                        g_Sentences.get( "role_menu_added_role", role.mention, Guild=interaction.guild_id ),
-                        silent=True,
-                        allowed_mentions=False
-                    );
-                #
-
-                await self.UpdateMenu( cache.ToDict, UserRoleMenu );
+                self.InteractionsOnChannels.pop( channel.id );
             #
         #
-        else:
-        #
-            cacheGuild = g_Cache.Plugin[ self.interaction.guild_id ][ menu ];
 
-            if not cacheGuild.IsEmpty:
-            #
-                embeds: list[ discord.Embed ] = [];
+        view = self.CRoleSelectionView( cache, fnCallback=self.SelectRole );
 
-                from src.Bot import bot;
-
-                for channel_ID, roleinfo in cacheGuild.items():
-                #
-                    validation = None;
-
-                    try:
-                        validation: discord.Message = await bot.get_channel( roleinfo[ "channel" ] ).fetch_message( channel_ID );
-                    except: pass;
-
-                    embeds.append(
-                        discord.Embed(
-                            color=RGB(0,100,0).hex,
-                            title=channel_ID,
-                            description=validation.jump_url if validation is not None else "null"
-                        )
-                    );
-                #
-                await interaction.response.send_message(
-                    g_Sentences.get( "role_menu_menus_limit", self.MaxMenus, Guild=interaction.guild_id ),
-                    embeds=embeds,
-                    silent=True,
-                    allowed_mentions=False
-                );
-
-                return;
-            #
-            UserRoleMenu = await interaction.channel.send( "✅" );
-            await interaction.response.send_modal( self.ConfigModal( interaction, UserRoleMenu, self ) );
-        #
+        self.InteractionsOnChannels[ channel.id ] = datetime.now();
+        await channel.send( view=view, allowed_mentions=False, mention_author=False, silent=True, delete_after=cache[ "life" ] );
     #
+
+    @Plugin.HandleExceptions()
+    async def command_roles( self, interaction: discord.Interaction ):
+        await self.RoleMenuInteraction( interaction.channel );
+        await interaction.response.send_message( "✅", ephemeral=True, delete_after=0.5 );
